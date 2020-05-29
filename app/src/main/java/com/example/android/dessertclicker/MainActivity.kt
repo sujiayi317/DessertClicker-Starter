ec605c1cb1a2647d8ea00284d73e52abbd5b30d8
sujiayi317 <sujiayi317@gmail.com>
2020-05-28 20:10:36 -0400

    Simulate app shutdown and use onSaveInstanceState()

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -27,6 +27,10 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
+
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
@@ -63,6 +67,44 @@ class MainActivity : AppCompatActivity() {
             Dessert(R.drawable.oreo, 6000, 20000)
     )
 
+    private var currentDessert = allDesserts[0]
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.i("onCreate Called")
+
+        // Use Data Binding to get reference to the views
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
+        dessertTimer = DessertTimer(this.lifecycle)
+
+        // common pattern for restoring data from the bundle:
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            dessertTimer.secondsCount =
+                    savedInstanceState.getInt(KEY_TIMER_SECONDS, 0)
+            showCurrentDessert()
+        }
+
+
+        // Set the TextViews to the right values
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        // Make sure the correct dessert is showing
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
+    }
+
     override fun onStart() {
         // to start the app and make it visible on the screen.
         super.onStart()
@@ -77,6 +119,7 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         Timber.i("onResume Called")
     }
+
 
     /*onPause() is called when the activity loses focus*/
     override fun onPause() {
@@ -93,37 +136,18 @@ class MainActivity : AppCompatActivity() {
         Timber.i("onStop Called")
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+        outState.putInt(KEY_TIMER_SECONDS, dessertTimer.secondsCount)
+
+        Timber.i("onSaveInstanceState Called")
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         Timber.i("onDestroy Called")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Timber.i("onRestart Called")
-    }
-
-    private var currentDessert = allDesserts[0]
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Timber.i("onCreate Called")
-
-        // Use Data Binding to get reference to the views
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.dessertButton.setOnClickListener {
-            onDessertClicked()
-        }
-
-        dessertTimer = DessertTimer(this.lifecycle)
-
-        // Set the TextViews to the right values
-        binding.revenue = revenue
-        binding.amountSold = dessertsSold
-
-        // Make sure the correct dessert is showing
-        binding.dessertButton.setImageResource(currentDessert.imageId)
     }
 
     /**
